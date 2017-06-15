dc0c20bad0bb4a576654b1c05df863273ff2d557
exceptionnotfound <mpjones428@gmail.com>
2017-06-15 10:53:39 -0700

    Added antiforgery tokens.

--- AspNetCoreModelBindingDemo/Startup.cs
+++ AspNetCoreModelBindingDemo/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreModelBindingDemo
 {
@@ -28,7 +29,10 @@ namespace AspNetCoreModelBindingDemo
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
